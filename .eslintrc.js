ff6906822f86fadf0f56897e43f2e01c42d74255
moodi <moodizone@gmail.com>
2025-04-27 13:02:34 +0330

    🔧 config linter for better performance

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: [
+    'expo',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react-hooks', '@typescript-eslint'],
   ignorePatterns: ['/dist/*'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
