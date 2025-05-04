c0a4ad0d4868147b9de025bf75b9670fb85110c8
moodi <moodizone@gmail.com>
2025-05-04 17:56:43 +0330

    🔧 config tailwind to compile hoc too

--- src/hoc/ScreenProvider.tsx
+++ src/hoc/ScreenProvider.tsx
@@ -1,15 +1,25 @@
+import { usePathname } from "expo-router";
 import { useColorScheme } from "nativewind";
 import * as React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { Button, SafeAreaView, StatusBar, Text, View } from "react-native";
 
 function ScreenProvider({ children }: React.PropsWithChildren) {
-  const { colorScheme } = useColorScheme();
+  const pathname = usePathname();
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <SafeAreaView className="flex-1 bg-white dark:bg-[#22343C]">
       <StatusBar
         barStyle={colorScheme === "light" ? "dark-content" : "light-content"}
         backgroundColor={colorScheme === "light" ? "#ffffff" : "#22343C"}
       />
+      <Text className="p-3 bg-red-300">{`Pathname: ${pathname}`}</Text>
+      <View className="p-3">
+        <Button
+          onPress={toggleColorScheme}
+          title={colorScheme ?? "toggle"}
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
       {children}
     </SafeAreaView>
   );
