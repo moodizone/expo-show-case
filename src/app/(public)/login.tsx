fd0730f5caa1cc46a28b2848b9524a5f2ea34290
moodi <moodizone@gmail.com>
2025-05-04 16:25:13 +0330

    🚧 add dummy login page

--- src/app/(public)/login.tsx
+++ src/app/(public)/login.tsx
@@ -1,8 +1,26 @@
 import * as React from "react";
-import { Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { Button, Text } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
 
-function login() {
-  return <Text>login page goes here</Text>;
+import ScreenProvider from "@/hoc/ScreenProvider";
+import { ROUTES } from "@/routes";
+
+function Login() {
+  const qc = useQueryClient();
+  const router = useRouter();
+  async function sett() {
+    await AsyncStorage.setItem("token", "token");
+    qc.setQueryData(["auth"], true);
+    router.push(ROUTES.home);
+  }
+  return (
+    <ScreenProvider>
+      <Text className="p-4 bg-red-300">login page goes here</Text>
+      <Button title="login" onPress={sett} />
+    </ScreenProvider>
+  );
 }
 
-export default login;
+export default Login;
