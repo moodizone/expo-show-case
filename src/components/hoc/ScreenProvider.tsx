2556ba2527ab171edcc49e7b455df2e3409b334c
moodi <moodizone@gmail.com>
2025-05-05 19:10:07 +0330

    ✨ improve ScreenProvider

--- src/components/hoc/ScreenProvider.tsx
+++ src/components/hoc/ScreenProvider.tsx
@@ -3,23 +3,35 @@ import { useColorScheme } from "nativewind";
 import * as React from "react";
 import { Button, SafeAreaView, StatusBar, Text, View } from "react-native";
 
-function ScreenProvider({ children }: React.PropsWithChildren) {
+interface Props {
+  hide?: boolean;
+  bg?: string;
+}
+
+function ScreenProvider({
+  children,
+  bg,
+  hide = false,
+}: React.PropsWithChildren<Props>) {
   const pathname = usePathname();
   const { colorScheme, toggleColorScheme } = useColorScheme();
+  const fallbackBg = colorScheme === "light" ? "#ffffff" : "#22343C";
   return (
     <SafeAreaView className="flex-1 bg-white dark:bg-[#22343C]">
       <StatusBar
         barStyle={colorScheme === "light" ? "dark-content" : "light-content"}
-        backgroundColor={colorScheme === "light" ? "#ffffff" : "#22343C"}
+        backgroundColor={bg ?? fallbackBg}
       />
-      <View className="p-3 justify-between flex-row gap-3">
-        <Text className="p-3 text-gray-950 dark:text-gray-50">{`Pathname: ${pathname}`}</Text>
-        <Button
-          onPress={toggleColorScheme}
-          title={colorScheme ?? "toggle"}
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
+      {hide ? null : (
+        <View className="p-3 justify-between flex-row gap-3">
+          <Text className="p-3 text-gray-950 dark:text-gray-50">{`Pathname: ${pathname}`}</Text>
+          <Button
+            onPress={toggleColorScheme}
+            title={colorScheme ?? "toggle"}
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+      )}
       {children}
     </SafeAreaView>
   );
