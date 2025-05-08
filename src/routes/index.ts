c87b0655ca6f2a6b78e950a202e358c0ba2e4955
moodi <moodizone@gmail.com>
2025-05-08 16:10:56 +0330

    🏗️ add register routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,31 @@ export const ROUTES = {
   home: "/",
   hi: "/hi",
   login: "/login",
-  register: "/register",
+  register: {
+    root: "/register",
+    phone: "/register/phone",
+    password: "/register/password",
+    faceId: "/register/faceId",
+  },
   onboarding: "/onboarding",
 } as const;
 
 export const publicRoutes = [
   ROUTES.login,
-  ROUTES.register,
+  ROUTES.register.root,
+  ROUTES.register.phone,
+  ROUTES.register.password,
+  ROUTES.register.faceId,
   ROUTES.onboarding,
   ROUTES.hi,
 ];
 
 // forbidden means user can not see them if has a auth session
-export const forbiddenPublics = [ROUTES.login, ROUTES.register, ROUTES.hi];
+export const forbiddenPublics = [
+  ROUTES.login,
+  ROUTES.register.root,
+  ROUTES.register.phone,
+  ROUTES.register.password,
+  ROUTES.register.faceId,
+  ROUTES.hi,
+];
