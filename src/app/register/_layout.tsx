9db57cc4254bc5d2f5db1d07a44a0770b9fcd644
moodi <moodizone@gmail.com>
2025-05-09 18:56:35 +0330

    🚧 registration first step

---
+++ src/app/register/_layout.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Stack } from "expo-router";
+import { useForm, FormProvider } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { signUpSchema } from "@/validations/auth";
+
+export type FormValues = z.infer<typeof signUpSchema>;
+
+function RegisterLayout() {
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      phone: "",
+      twoFACode: "",
+    },
+    mode: "onBlur",
+    shouldFocusError: false,
+    resolver: zodResolver(signUpSchema),
+  });
+
+  return (
+    <FormProvider {...methods}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="phone" />
+        <Stack.Screen name="password" />
+        <Stack.Screen name="faceId" />
+      </Stack>
+    </FormProvider>
+  );
+}
+
+export default RegisterLayout;
---
+++ src/app/register/index.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Controller, useFormContext } from "react-hook-form";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { useRouter } from "expo-router";
+
+import AuthLayout from "@/components/screens/auth/layout";
+import Input from "@/components/input";
+import { FormValues } from "@/app/register/_layout";
+import Footer from "@/components/screens/auth/footer";
+import LabelIcon from "@/components/screens/auth/labelIcon";
+import { ROUTES } from "@/routes";
+
+function RegisterRoot() {
+  const {
+    formState: { errors },
+    control,
+  } = useFormContext<FormValues>();
+  const router = useRouter();
+
+  return (
+    <AuthLayout title="Hello!" description="lets introduce">
+      <View>
+        <View className="flex-row gap-x-2.5 items-start">
+          <View className="flex-grow-0">
+            <LabelIcon
+              icon={
+                <FontAwesome6 name="user-large" size={16} color="#FFC542" />
+              }
+            />
+          </View>
+          <View className="flex-grow">
+            <Controller
+              control={control}
+              render={({ field: { ref, onChange, ...others } }) => (
+                <Input
+                  {...others}
+                  onChangeText={onChange}
+                  placeholder="Your full name"
+                  className="mb-9"
+                  error={errors.name?.message}
+                />
+              )}
+              name="name"
+            />
+          </View>
+        </View>
+        <View className="flex-row gap-x-2.5 items-start">
+          <View className="flex-grow-0">
+            <LabelIcon
+              icon={
+                <FontAwesome6 name="user-large" size={16} color="#FFC542" />
+              }
+            />
+          </View>
+          <View className="flex-grow">
+            <Controller
+              control={control}
+              render={({ field: { ref, onChange, ...others } }) => (
+                <Input
+                  {...others}
+                  onChangeText={onChange}
+                  placeholder="Email address"
+                  error={errors.email?.message}
+                  keyboardType="email-address"
+                />
+              )}
+              name="email"
+            />
+          </View>
+        </View>
+        <View className="mt-16">
+          <Footer
+            backHandler={() => {
+              if (router.canGoBack()) {
+                router.back();
+              } else {
+                router.push(ROUTES.login);
+              }
+            }}
+            nextHandler={() => router.push(ROUTES.register.password)}
+          />
+        </View>
+      </View>
+    </AuthLayout>
+  );
+}
+
+export default RegisterRoot;
