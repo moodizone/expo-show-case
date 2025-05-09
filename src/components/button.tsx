9c8ab5b414d612ac466823f43455025a6c6a3eb4
moodi <moodizone@gmail.com>
2025-05-09 16:00:14 +0330

    🚧 add basic and icon btn

---
+++ src/components/button.tsx
@@ -0,0 +1,142 @@
+import { darkenHexColor } from "@/utils/color";
+import * as React from "react";
+import {
+  ActivityIndicator,
+  Pressable,
+  PressableProps,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { twMerge } from "tailwind-merge";
+
+const duration = 200;
+
+interface BasicButtonProps extends PressableProps {
+  variant?: "solid" | "text";
+  loading?: boolean;
+  bgColor?: string;
+  accent?: string;
+  width?: number;
+  shadow?: {
+    color: string;
+    opacity: number;
+  };
+}
+
+export function Button({
+  variant = "solid",
+  bgColor = "#3DD598", // meadow-300
+  accent = "#fff",
+  shadow,
+  disabled,
+  loading,
+  style,
+  className,
+  onPressIn,
+  onPressOut,
+  children,
+  ...others
+}: BasicButtonProps) {
+  const normalBg = variant === "solid" ? bgColor : "#00000000"; // transparent
+  const pressedBg =
+    variant === "solid" ? darkenHexColor(bgColor, 0.1) : "#BEC7C5";
+  const isDisabled = disabled ? "opacity-60" : "opacity-100";
+  const shadowStyle = shadow
+    ? {
+        shadowColor: shadow.color,
+        shadowOpacity: shadow.opacity,
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowRadius: 4,
+        elevation: 4,
+      }
+    : undefined;
+  const transition = useSharedValue(0);
+  const isActive = useSharedValue(false);
+  const animatedStyle = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(
+      transition.value,
+      [0, 1],
+      [normalBg, pressedBg]
+    ),
+  }));
+
+  return (
+    <Pressable
+      {...others}
+      style={style}
+      className="w-full"
+      disabled={disabled || loading}
+      onPressIn={(e) => {
+        isActive.value = true;
+        transition.value = withTiming(1, { duration }, () => {
+          if (!isActive.value) {
+            transition.value = withTiming(0, { duration });
+          }
+        });
+        onPressIn?.(e);
+      }}
+      onPressOut={(e) => {
+        if (transition.value === 1) {
+          transition.value = withTiming(0, { duration });
+        }
+        isActive.value = false;
+        onPressOut?.(e);
+      }}
+      accessible={true}
+      accessibilityRole="button"
+      accessibilityLabel="Press me"
+    >
+      <Animated.View
+        style={[shadowStyle, animatedStyle]}
+        className={twMerge(
+          `justify-center items-center rounded-[12px] h-[60px] w-full`,
+          isDisabled,
+          className
+        )}
+      >
+        {loading ? (
+          <ActivityIndicator color={accent} size={20} />
+        ) : (
+          (children as React.ReactNode)
+        )}
+      </Animated.View>
+    </Pressable>
+  );
+}
+
+interface IconButtonProps extends BasicButtonProps {
+  icon: React.ReactNode;
+  rounded?: boolean;
+  size?: number;
+}
+
+export function IconButton({
+  icon,
+  rounded = false,
+  size = 60,
+  ...others
+}: IconButtonProps) {
+  const radiusStyles = rounded ? "rounded-[50%]" : undefined;
+
+  return (
+    <Button
+      {...others}
+      className={radiusStyles}
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      {icon}
+    </Button>
+  );
+}
